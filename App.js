27c2ebfcd7c7e8592723765fbabf8ae3efe732fb
EduardoHobold <38993334+EduardoHobold@users.noreply.github.com>
2019-05-28 19:57:27 -0300

    'Inicio'

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Text, TextInput, View} from 'react-native';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>DALEEE</Text>
+      </View>
+    )
+  }
+}
+
