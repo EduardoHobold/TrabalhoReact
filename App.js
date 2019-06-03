2851de5fe2a4add274dd022be1193b3dc94d62d5
EduardoHobold <38993334+EduardoHobold@users.noreply.github.com>
2019-06-02 21:17:31 -0300

    Projeto

--- App.js
+++ App.js
@@ -1,14 +1,83 @@
-
 import React from 'react';
-import { Text, TextInput, View} from 'react-native';
+import { Text, TextInput, View, TouchableHighlight} from 'react-native';
+import StyleSheet from './src/estilos/Padrao'
 
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = { numeros: 0, indice: 0, trocas: 0, comparacoes: 0}
+  }
+
+    maisUm = () => {
+
+        var s = this.state
+
+        var vetor = [];
+        s.numeros.replace(" ", ",");
+        s.numeros.replace("-", ",");
+        vetor = s.numeros.split(",")
+
+        var tamanho = vetor.length-1;
+        var tamanhoB = vetor.length-1;
+
+        s.indice = tamanho;
+
+      for(let i = 0;i < tamanho;i++){
+        for(let x = 0;x < tamanhoB;x++){
+            if(vetor[x] > vetor[x+1]){
+                temp1 = vetor[x];
+                temp2 = vetor[x+1];
+                vetor[x] = temp2;
+                vetor[x+1] = temp1;
+                s.comparacoes++;
+                s.trocas++;
+            }else{
+                s.comparacoes++;
+            } 
+        }
+        tamanhoB--;
+      }
+
+      this.setState(s);
+  }
+
+  limpar = () => {
+      var s = this.state
+       
+      s.indice = 0;
+      s.trocas = 0;
+      s.comparacoes = 0;
+
+      this.setState(s);
+  }
+
   render() {
     return (
       <View>
-        <Text>DALEEE</Text>
-      </View>
+        <View>
+          <Text style={StyleSheet.titulo}>Trabalho Estrutura de Dados</Text>
+          <TextInput placeholder="Informe os números" keyboardType="numeric" style={StyleSheet.input}
+          onChangeText={ (numeros) => this.setState({numeros})}></TextInput>
+        </View>
+
+        <View>
+          <TouchableHighlight
+            onPress={this.maisUm}
+            onLongPress={this.limpar}>
+            <Text>Calcular</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View>
+          <Text style={StyleSheet.titulo}>{this.state.indice}</Text>
+          <Text style={StyleSheet.titulo}>{this.state.trocas}</Text>
+          <Text style={StyleSheet.titulo}>{this.state.comparacoes}</Text>
+        </View>
+
+     </View>
+
     )
   }
 }
--- src/estilos/Padrao.js
+++ src/estilos/Padrao.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    ex:{
+    titulo:{
        paddingHorizontal: 15,
        marginVertical: 5,
        borderRadius: 10,
@@ -9,5 +9,12 @@ export default StyleSheet.create({
        borderColor: '#222',
        fontSize: 24,
        fontWeight: 'bold',
-    }
+    },
+    input:{
+        height:80,
+        width:"100%",
+        fontSize:36,
+        marginTop:10,
+        borderWidth: 2,
+      }
 })
